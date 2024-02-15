2228fafbc6161fb7cb9a060a9b51fffa25b9c34b
Basila Bob <terencemusimbi1@gmail.com>
2024-02-15 14:32:54 +0100

    gestion system version 1

---
+++ gestionsystemexercice.py
@@ -0,0 +1,9 @@
+import os
+
+def new_user():
+    confirm = "N"
+    while confirm != "Y":
+          username = input("Indiquez le nom de l'utilisateur à ajouter : ")
+          print("Utiliser username '" + username + "'? (Y/N)")
+          confirm = input().upper()
+    os.system("sudo adduser " + username)
